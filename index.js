03589e1f8496e48ee01e472e0ae892a09565bba7
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-13 06:31:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const connection = mysql.createConnection({
     database: 'employeeDB',
 });
 
-// Connecting to MySQL Server & SQL Database
 connection.connect((err) => {
     if (err) throw err;
     start();
@@ -40,10 +39,87 @@ const start = () => {
 }
 const add = () => {
     console.log("add!")
-}
+    
+    inquirer
+        .prompt([
+            {
+                name: 'addX',
+                message: "Would you like to add...?",
+                type: 'list',
+                choices: ['A Department', 'A Role', 'An Employee', 'EXIT'],
+            }
+        ])
+        .then((answer => {
+            switch (answer.addX) {
+                case 'A Department':
+                    addDepartment();
+                    break;
+                case 'A Role':
+                    addRole();
+                    break;
+                case 'An Employee':
+                    addEmployee();
+                    break;
+                case 'EXIT':
+                    connection.end();
+                    break;
+            }
+        }))
+};
+
 const view = () => {
     console.log("view!")
+
+    inquirer
+        .prompt([
+            {
+                name: 'viewX',
+                message: "Would you like to view...?",
+                type: 'list',
+                choices: ['A Department', 'A Role', 'An Employee', 'EXIT'],
+            }
+        ])
+        .then((answer => {
+            switch (answer.viewX) {
+                case 'A Department':
+                    viewDepartment();
+                    break;
+                case 'A Role':
+                    viewRole();
+                    break;
+                case 'An Employee':
+                    viewEmployee();
+                    break;
+                case 'EXIT':
+                    connection.end();
+                    break;
+            }
+        }))
 }
+const addDepartment = () => {
+    console.log("addDepartment!")
+}
+
+const addRole = () => {
+    console.log("addRole!")
+}
+
+const addEmployee = () => {
+    console.log("addEmployee!")
+}
+
+const viewDepartment = () => {
+    console.log("viewDepartment!")
+}
+
+const viewRole = () => {
+    console.log("viewRole!")
+}
+
+const viewEmployee = () => {
+    console.log("viewEmployee!")
+}
+
 const update = () => {
     console.log("update!")
 }
