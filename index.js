f2e6d1c7d8e4526f851d620879ee0f4075b38961
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-13 06:08:36 -0500

    Set up Inquirer

--- index.js
+++ index.js
@@ -3,19 +3,47 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'Kitty08967!',
-  database: 'employeeDB',
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Kitty08967!',
+    database: 'employeeDB',
 });
 
 // Connecting to MySQL Server & SQL Database
 connection.connect((err) => {
-  if (err) throw err;
-  start();
+    if (err) throw err;
+    start();
 });
 
 const start = () => {
-
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: "Would you like to add, view, or update?",
+                name: 'task',
+                choices: ['add', 'view', 'update', 'EXIT'],
+            }
+        ])
+        .then((answer) => {
+            if (answer.task === 'add') {
+                add();
+            } else if (answer.task === 'view') {
+                view();
+            } else if (answer.task === 'update') {
+                update();
+            } else if (answer.task === 'EXIT') {
+                connection.end();
+            }
+        });
+}
+const add = () => {
+    console.log("add!")
+}
+const view = () => {
+    console.log("view!")
+}
+const update = () => {
+    console.log("update!")
 }
